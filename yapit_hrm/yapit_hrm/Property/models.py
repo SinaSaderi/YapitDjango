d5887ab7f25a14e53df1cbc2d6c31e163aeb2667
Sina Saderi <sina.saderi@gmail.com>
2024-11-30 21:15:44 +0330

    Add yapit_hrm/Property/models.py

---
+++ yapit_hrm/yapit_hrm/Property/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Property(models.Model):
+    type = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+
+
+    # Fields
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return str(self.pk)
+
+    def get_absolute_url(self):
+        return reverse("SampleModel_SampleModel_detail", args=(self.pk,))
+
+    def get_update_url(self):
+        return reverse("SampleModel_SampleModel_update", args=(self.pk,))
+
